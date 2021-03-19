c46285ee10cc73c0d0bebb5f70865bb338ea1961
FlapjaxAttack <flapjack.mail@gmail.com>
2021-03-18 23:00:41 -0600

    Foreach Loops File Practice

--- SidePlatformer/Assets/Scripts/Practice/ForeachLoopswithArray.cs
+++ SidePlatformer/Assets/Scripts/Practice/ForeachLoopswithArray.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ForeachLoopswithArray : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
+        string[] foodStuffs = {"pizza", "bagels", "pasta", "salad"};
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (string i in foodStuffs) 
+        {
+            Console.WriteLine(i);
+        }
     }
 }
